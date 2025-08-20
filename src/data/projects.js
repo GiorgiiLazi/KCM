da0b28078fdc8abd746b14918f6596dc5e286458
GiorgiiLazi <malazi140@gmail.com>
2025-08-20 23:16:58 +0400

    fixed issue with export

---
+++ src/data/projects.js
@@ -0,0 +1,56 @@
+export const projects = Object.freeze([
+  {
+    id: 'dom-1',
+    name: 'Одноэтажный дом 1',
+    square: 120,
+    floors: 1,
+    rooms: 3,
+    height: 6,
+    images: [
+      '/images/projects/dom1-1.webp',
+      '/images/projects/dom1-2.webp',
+      '/images/projects/dom1-3.webp'
+    ],
+    plans: [
+      '/images/plans/dom1-1.webp',
+      '/images/plans/dom1-2.webp'
+    ],
+    description: 'Просторный одноэтажный дом с современным дизайном и функциональной планировкой.'
+  },
+  {
+    id: 'dom-2',
+    name: 'Двухэтажный дом 2',
+    square: 200,
+    floors: 2,
+    rooms: 5,
+    height: 8,
+    images: [
+      '/images/projects/dom2-1.webp',
+      '/images/projects/dom2-2.webp',
+      '/images/projects/dom2-3.webp'
+    ],
+    plans: [
+      '/images/plans/dom2-1.webp',
+      '/images/plans/dom2-2.webp',
+      '/images/plans/dom2-3.webp'
+    ],
+    description: 'Двухэтажный дом с большими окнами и уютной террасой, идеально для семьи.'
+  },
+  {
+    id: 'dom-3',
+    name: 'Одноэтажный дом 3',
+    square: 150,
+    floors: 1,
+    rooms: 4,
+    height: 6.5,
+    images: [
+      '/images/projects/dom3-1.webp',
+      '/images/projects/dom3-2.webp'
+    ],
+    plans: [
+      '/images/plans/dom3-1.webp',
+      '/images/plans/dom3-2.webp'
+    ],
+    description: 'Современный одноэтажный коттедж с просторной кухней-гостиной и двумя спальнями.'
+  }
+])
